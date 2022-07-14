cb4308e0923fe8af46927386e744e9c2b2257570
Koval Bohdan [PC] <workbohdankoval@gmail.com>
2022-07-14 14:54:07 +0300

    Added the ability to work with Celery asynchronous tasks.

---
+++ myshop/celery.py
@@ -0,0 +1,11 @@
+import os
+from celery import Celery
+
+
+# Задаем переменную окружения, содержащую название файла настроек нашего проекта.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+
+app = Celery('myshop')
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks()
