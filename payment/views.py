c2efa69e19977a3e7c046fe2ac930e8e2e015392
Koval Bohdan [PC] <workbohdankoval@gmail.com>
2022-07-15 19:24:27 +0300

    The provision of a receipt when purchasing a PDF has been added and a full-fledged coupon system has been added

--- payment/views.py
+++ payment/views.py
@@ -1,12 +1,73 @@
 from django.shortcuts import render, redirect, get_object_or_404
 import braintree
 from orders.models import Order
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+import weasyprint
+from io import BytesIO
 
 
 def payment_process(request):
     order_id = request.session.get('order_id')
     order = get_object_or_404(Order, id=order_id)
-    
+
+    if request.method == 'POST':
+        # retrieve nonce
+        nonce = request.POST.get('payment_method_nonce', None)
+        # create and submit transaction
+        result = braintree.Transaction.sale({
+            'amount': '{:.2f}'.format(order.get_total_cost()),
+            'payment_method_nonce': nonce,
+            'options': {
+                'submit_for_settlement': True
+            }
+        })
+        if result.is_success:
+            # mark the order as paid
+            order.paid = True
+            # store the unique transaction id
+            order.braintree_id = result.transaction.id
+            order.save()
+
+            # Создание электронного сообщения.
+            subject = 'My Shop - Invoice no. {}'.format(order.id)
+            message = 'Please, find attached the invoice for your recent purchase.'
+            email = EmailMessage(subject,
+                                 message,
+                                 'admin@myshop.com',
+                                 [order.email])
+
+            # Формирование PDF.
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(
+                settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out,
+                                                   stylesheets=stylesheets)
+            # Прикрепляем PDF к электронному сообщению.
+            email.attach('order_{}.pdf'.format(order.id),
+                         out.getvalue(),
+                         'application/pdf')
+            # Отправка сообщения.
+            email.send()
+
+            return redirect('payment:done')
+        else:
+            return redirect('payment:canceled')
+    else:
+        # generate token
+        client_token = braintree.ClientToken.generate()
+        return render(request,
+                      'payment/process.html',
+                      {'order': order,
+                       'client_token': client_token})
+
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+
     if request.method == 'POST':
         # Получение токена для создания транзакции.
         nonce = request.POST.get('payment_method_nonce', None)
@@ -17,7 +78,7 @@ def payment_process(request):
             'options': {
                 'submit_for_settlement': True
             }
-    })
+        })
         if result.is_success:
             # Отметка заказа как оплаченного.
             order.paid = True
@@ -35,7 +96,10 @@ def payment_process(request):
                   {'order': order,
                    'client_token': client_token})
 
+
 def payment_done(request):
     return render(request, 'payment/done.html')
+
+
 def payment_canceled(request):
     return render(request, 'payment/canceled.html')
